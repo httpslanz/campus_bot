6678527e6a1e111d62221d547e33f3d181ec58fa
Lanz Aquino <lanzaquino23@gmail.com>
2026-03-04 18:48:06 +0800

    Fix CSRF trusted origins for Railway

--- campusbot/settings.py
+++ campusbot/settings.py
@@ -33,6 +33,10 @@ DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '*').split(',')
 
+CSRF_TRUSTED_ORIGINS = [
+    "https://chatbot-lcc.up.railway.app",
+]
+
 
 AUTH_USER_MODEL = 'chatbot.User'
 
